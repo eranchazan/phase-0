013fe76e1e63434d00d1b5b47419214c9b707a0c
Eran Chazan <eranchazan@gmail.com>
2015-09-30 19:37:53 -0700

    adding 4.3 reflection file

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -13,3 +13,22 @@ add_one = numbers + 1
 character = add_one.to_s
 puts "Here's an even better number" + character 
 
+=begin
+
+How do you define a local variable?
+To define a local variable you type the variable name you want to us followed by the equal sign followed by the value or object the variable represents. 
+
+How do you define a method?
+To define a method you type def followed by the method name followed by the number of arguments in parentheses. 
+
+What is the difference between a local variable and a method?
+A local variable is a variable only recognized by the file it is written in. It's just a placeholder that represents a copy of the block of code that can be reused and calculated on in other parts of the file. 
+A method is a function that accepts input and spits out an output based on the code you apply on the input in the body. It can be recognized globally.  
+
+How do you run an RSpec file from the command line?
+rspec filename
+
+What was confusing about this material? What made sense?
+What was confusing at first was how to add spaces when printing lines of strings to the console. 
+What made sense was how it is very important to apply .to_s to integers in puts statements. 
+=end
