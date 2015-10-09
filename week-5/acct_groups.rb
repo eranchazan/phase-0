7d0fbb220e5505f395028c865003dbf885f0eefb
Eran Chazan <eranchazan@gmail.com>
2015-10-08 22:59:22 -0700

    initial set up commit

---
+++ week-5/acct_groups.rb
@@ -0,0 +1,22 @@
+copperheads = ["Joshua_Abrams", "Syema_Ailia", "Kris_Bies", "Alexander_Blair", "Andrew_Blum",
+ "Jacob_Boer", "Stevenz_Broderick", "Ovi_Calvo", "Danielle_Cameron", "Eran_Chazan", "Jonathan_Chen", 
+ "Un_Choi", "Kevin_Corso", "Eric_Dell'Aringa", "Eunice_Do", "Ronny_Ewanek", "John_Paul", "Chaufan_Field",
+ "Eric_Freeburg", "Jefferey_George", "Jamar_Gibbs", "Paul_Gaston_Gouron", "Gabrielle_Gustilo", 
+ "Marie-France_Han", "Noah_Heinrich", "Jack_Huang", "Max_Iniguez", "Mark_Janzer", "Michael_Jasinski",
+ "Lars_Johnson", "Joshua_Kim", "James_Kirkpatrick", "Christopher_Lee", "Isaac_Lee", "Joseph_Marion", "Kevin_Mark", 
+ "Bernadette_Masciocchi", "Bryan_Munroe", "Becca_Nelson", "Van_Phan", "John_Polhill", "Jeremy_Powell", 
+ "Jessie_Richardson", "David_Roberts", "Armani_Saldana", "Chris_Savage", "Parminder_Singh", "Kyle_Smith", "Aaron_Tsai",
+  "Douglas_Tsui", "Deanna_Warren", "Peter_Wiebe", "Daniel_Woznicki", "Jay_Yee", "Nicole_Yee", "Bruno_Zatta"]
+
+
+
+# PseudoCode
+# Accept an array
+# Go through every element in the array 
+# Split up the array into smaller arrays after every fourth index into smaller arrays
+# Return a new set of individual arrays with four names in each index 
+
+def acct_group (array)
+
+
+end
