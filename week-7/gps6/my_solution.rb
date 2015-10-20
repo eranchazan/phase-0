7893d35884a6597484e0449f7b87c3f8d6c30baf
Eran Chazan <eranchazan@gmail.com>
2015-10-19 18:09:01 -0700

    adding gps3.1

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -9,52 +9,53 @@
 require_relative 'state_data'
 
 class VirusPredictor
-
+  # initialize and identify the instance variables in this class
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+  # calls the other two methods
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
-
-  def predicted_deaths(population_density, population, state)
+  # calculates predicted deaths based on data from the state_data.rb file
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
+    case @population_density
+    when@population_density >= 200
       number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
+     when @population_density >= 150
       number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
+    when @population_density >= 100
       number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
+    when @population_density >= 50
       number_of_deaths = (@population * 0.1).floor
-    else
+    when
       number_of_deaths = (@population * 0.05).floor
     end
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
-
-  def speed_of_spread(population_density, state) #in months
+  # calculates speed of spread based 
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
     speed = 0.0
-
-    if @population_density >= 200
+    case @population_density
+    when @population_density >= 200
       speed += 0.5
-    elsif @population_density >= 150
+    when @population_density >= 150
       speed += 1
-    elsif @population_density >= 100
+    when @population_density >= 100
       speed += 1.5
-    elsif @population_density >= 50
+    when @population_density >= 50
       speed += 2
-    else
+    when
       speed += 2.5
     end
 
@@ -62,16 +63,19 @@ class VirusPredictor
 
   end
 
+
 end
 
 #=======================================================================
 
 # DRIVER CODE
  # initialize VirusPredictor for each state
-
+STATE_DATA.each do |state, data|
+  state = VirusPredictor.new(state, data[:population_density], data[:population]) 
+  state.virus_effects 
+end
 
 alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
 
 jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
 jersey.virus_effects
@@ -81,3 +85,16 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
+#Reflection
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+#What does require_relative do? How is it different from require?
+#What are some ways to iterate through a hash?
+#When refactoring virus_effects, what stood out to you about the variables, if anything?
+#What concept did you most solidify in this challenge?
+#
+#
+#
+#
+#
+
+
